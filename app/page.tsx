34fbfb2a1887e96d8e28d5218df24b131679381d
HARIHARAN B P <bphariharan1301@gmail.com>
2025-09-11 16:47:14 +0530

    Round 1 task

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,153 @@
+'use client'
 import { redirect } from "next/navigation";
+import { useState } from "react";
+
+
+const mockData = [
+	{
+		id: '1',
+		name: 'Dashboard',
+		type: 'folder',
+		child: [
+			{
+				id: '1.1',
+				name: 'child-1',
+				type: 'file'
+			},
+			{
+				id: '1.2',
+				name: 'child-2',
+				type: 'folder',
+				child: [
+					{
+						id: '1.2.1',
+						name: 'child-2-1',
+						type: 'file'
+					},
+					{
+						id: '1.2.2',
+						name: 'child-2-2',
+						type: 'file'
+					},
+				]
+			},
+			{
+				id: '1.3',
+				name: 'temp',
+				type: 'file'
+			},
+		]
+	},
+	{
+		id: '2',
+		name: 'Dashboard-2',
+		type: 'folder',
+		child: [
+			{
+				id: '2.1',
+				name: 'temp',
+				type: 'file'
+			},
+			{
+				id: '2.2',
+				name: 'child-2',
+				type: 'folder',
+				child: [
+					{
+						id: '2.2.1',
+						name: 'temp',
+						type: 'file'
+					},
+					{
+						id: '2.2.2',
+						name: 'child-2',
+						type: 'file'
+					},
+				]
+			},
+			{
+				id: '2.3',
+				name: 'temp',
+				type: 'file'
+			},
+		]
+	},
+]
+
+// function ChildComponent(props: any) {
+
+// 	const { childData } = props
+// 	console.log('childData', childData)
+
+// 	return (
+// 		<>
+// 			{
+// 				childData?.map((data: any, index: any) => (
+// 					<div key={index} >
+// 						{data.name}
+// 						{
+// 							data.child?.length > 0 && (
+// 								<ChildComponent childData={data.child} />
+// 							)
+// 						}
+// 					</div>
+// 				))
+// 			}
+// 		</>
+// 	)
+// }
+
+
+function FolderComponent(props: any) {
+	const { data } = props
+	const [open, setOpen] = useState(false)
+	return (
+		<>
+			{data.type === 'folder' && (<button onClick={() => setOpen((prev) => !prev)}>
+				{open ? 'Close' : 'Open'}
+			</button>)}
+			{
+				open ? (
+					<>
+						<div>
+							{data.name}
+							{
+								data.child?.length > 0 && (
+									data.child.map((child: any, index: any) => (
+										<FolderComponent key={index} data={child} />
+									))
+								)
+							}
+						</div>
+						{'-----------------------------------------------'}
+					</>
+				) : (
+					<>
+						<div>
+							{data.name}
+						</div>
+					</>
+				)
+			}
+		</>
+	)
+}
 
 export default function Home() {
-	redirect("/dashboard");
+	// redirect("/dashboard");
+
+	return (
+		<div>
+			{
+				mockData.map((data, index) => (
+					<div key={index}>
+						<FolderComponent data={data} />
+					</div>
+				))
+			}
+		</div>
+	)
+
+
+
 }
